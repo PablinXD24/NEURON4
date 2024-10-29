b11c4facbfad5241fecfa7b7e06771f486f00662
PablinXD24 <pablo11ssousa2@gmail.com>
2024-10-29 16:40:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Adiciona um índice a cada letra para controlar o delay na animação
+document.querySelectorAll('.letter').forEach((letter, index) => {
+  letter.style.setProperty('--index', index);
+});
+setTimeout(() => {
+    // Altere o link abaixo para o seu URL desejado
+    window.location.href = "https://pablinxd24.github.io/NEURON3/"; 
+}, 4000); // Tempo total da animação mais 2 segundos
+}, 4000); // Tempo total da animação mais 2 segundos
